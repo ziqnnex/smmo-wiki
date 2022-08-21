ea0348bd3e2061703b87f5e24e3865e7a4b39869
Y0mu <49496357+ImY0mu@users.noreply.github.com>
2022-08-21 12:19:13 +0200

    Add files via upload

---
+++ lang/cz/titles.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for titles at specific predefined pages like item search, best in slot, etc.
+    |
+    */
+    'search_items' => 'Vyhledat předměty',
+    'best_in_slot' => 'Nejlěpší v slotu',
+];
