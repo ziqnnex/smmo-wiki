f1a1dddb6ca23795d7b1763e0e87ad5e4c481e27
Y0mu <49496357+ImY0mu@users.noreply.github.com>
2022-08-21 16:26:15 +0200

    Add files via upload

---
+++ lang/en/bis_search.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for best in slot search page.
+    |
+    */
+    'look_for_potential_best_items_for_you' => 'Look for potential best items for you!',
+    'strength_defence_or_crit' => 'strength, defence or crit?',
+];
---
+++ lang/en/item_types.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for item types.
+    |
+    */
+    'weapon' => 'Weapon',
+    'armour' => 'Armour',
+    'special' => 'Special',
+    'helmet' => 'Helmet',
+    'amulet' => 'Amulet',
+    'shield' => 'Shield',
+    'greaves' => 'Greaves',
+    'gauntlet' => 'Gauntlet',
+    'boots' => 'Boots',
+    'pet' => 'Pet',
+];
