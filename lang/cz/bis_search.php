de9eb675510488b59a2cc872a14856b425ffe0dc
Y0mu <49496357+ImY0mu@users.noreply.github.com>
2022-08-21 16:26:00 +0200

    Add files via upload

---
+++ lang/cz/bis_search.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for best in slot search page.
+    |
+    */
+    'look_for_potential_best_items_for_you' => 'Podívejte se na potenciální nejlepší předměty pro vás!',
+    'strength_defence_or_crit' => 'Síla, obrana nebo kritický zásah?',
+];
---
+++ lang/cz/item_types.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for item types.
+    |
+    */
+    'weapon' => 'Zbraň',
+    'armour' => 'Brnění',
+    'special' => 'Speciální',
+    'helmet' => 'Helma',
+    'amulet' => 'Amulet',
+    'shield' => 'Štít',
+    'greaves' => 'Holenice',
+    'gauntlet' => 'Rukavice',
+    'boots' => 'Boty',
+    'pet' => 'Mazlíček',
+];
