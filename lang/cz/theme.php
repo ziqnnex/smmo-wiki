bcd0b425277924e60d36af2cb0ef398e01ae4131
Y0mu <49496357+ImY0mu@users.noreply.github.com>
2022-08-21 10:51:19 +0200

    Create theme.php

---
+++ lang/cz/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used at theme button.
+    |
+    */
+    'theme' => 'Téma',
+    'dark' => 'Tmavé',
+    'light' => 'Světlé',
+    'device' => 'Zařízení',
+];
