041c22fc1eef7dad3e3fc1f78eda7041c3512e4e
Y0mu <49496357+ImY0mu@users.noreply.github.com>
2022-08-21 10:51:35 +0200

    Create theme.php

---
+++ lang/en/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used at theme button.
+    |
+    */
+    'theme' => 'Theme',
+    'dark' => 'Dark',
+    'light' => 'Light',
+    'device' => 'Device',
+];
